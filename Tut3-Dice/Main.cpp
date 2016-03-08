8ae080d7194748e8041637a1b220ae46f112fe5a
Jonathan Kellermann <jonathankellermann75@gmail.com>
2016-03-08 09:36:07 +0200

    Function and class created

--- Tut3-Dice/Main.cpp
+++ Tut3-Dice/Main.cpp
@@ -1,3 +1,44 @@
 #include <iostream>
 #include <time.h>
 #include <cstdlib>
+
+
+class Dice{
+	
+	
+public:
+	Dice(){
+		srand(time(NULL));
+	}
+
+	int roll()
+	{
+		return rand() % 6 + 1;
+	};
+
+
+	float Average(Dice myDice, int rolls)
+	{
+	
+		return
+	};
+
+	float Average(int DiceArray[], int rolls)
+	{
+		int sum;
+		for (int i = 0; i < rolls; i++)
+		{
+			sum += DiceArray[i];
+		}
+		return sum/rolls;
+	};
+
+	
+
+};
+
+int main()
+{
+	
+	return 0;
+};
